0abe2f7d08be6d88a794d78a602f34056e756877
Fernando Benavides <nockzblack@gmail.com>
2022-04-04 12:42:37 -0500

    Initial Commit

---
+++ CombineExample/CombineExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineExampleApp.swift
+//  CombineExample
+//
+//  Created by Fernando's Mac on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CombineExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
