6df9b921ee357a8221859591a8e6839f4832db5e
Fernando Benavides <nockzblack@gmail.com>
2022-04-04 12:56:24 -0500

    Model and View Build

--- CombineExample/ContentView.swift
+++ CombineExample/ContentView.swift
@@ -5,7 +5,74 @@
 //  Created by Fernando's Mac on 04/04/22.
 //
 
+
+// MVVM Model-View-ViewModel
+
 import SwiftUI
+import Combine
+
+// Model:
+
+struct User: Identifiable, Hashable, Decodable {
+    let id: Int
+    let name: String
+    
+}
+
+// ViewModel:
+
+final class ViewModel: ObservableObject {
+   @Published var time = ""
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    @Published var users = [User]()
+    
+    let formatter: DateFormatter = {
+        let df = DateFormatter()
+        df.timeStyle = .medium
+        return df
+    }()
+    
+    init() {
+        setupPublishers()
+    }
+    
+    private func setupPublishers() {
+        setupTimer()
+        setupDataTaskPublisher()
+    }
+    
+    private func setupDataTaskPublisher() {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/users")!
+        
+        URLSession.shared.dataTaskPublisher(for: url)
+            .tryMap { (data, response) in
+                guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+                
+            }
+            .decode(type: [User].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: {_ in}) { users in
+                self.users = users
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func setupTimer() {
+        Timer.publish(every: 1, on: .main, in: .default)
+            .autoconnect()
+            .receive(on: RunLoop.main)
+            .sink { value in
+                self.time = self.formatter.string(from: value)
+            }
+            .store(in: &cancellables)
+    }
+}
+
 
 struct ContentView: View {
     var body: some View {
